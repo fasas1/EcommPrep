3b3417fa4549bd6a208d45794c19a3f7cfbd9aa3
Fasasi Qasim <fasaqa1000@gmail.com>
2025-09-16 13:23:31 +0100

    added FLUENT API

--- Entities/Customer.cs
+++ Entities/Customer.cs
@@ -2,5 +2,15 @@
 {
     public class Customer
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+        public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
+
+        // Computed property
+        public string FullName => $"{FirstName} {LastName}";
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Ecomm_demo.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,25 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"), sqlServerOptions =>
+    {
+        sqlServerOptions.EnableRetryOnFailure(
+            maxRetryCount: 5, // Number of retry attempts
+            maxRetryDelay: TimeSpan.FromSeconds(10), // Delay between retries
+            errorNumbersToAdd: null); // List of additional error numbers to consider transient
+    });
+});
+
+
+
+
+
+
+
+
 
 var app = builder.Build();
 
