5b8d2cbed2477fd994ea69c79fa7dda231cf9fef
Fasasi Qasim <fasaqa1000@gmail.com>
2025-09-18 12:14:38 +0100

    added Automapper

---
+++ MappingConfig.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using DesignArch.Entities;
+using DesignArch.Entities.Dto;
+
+namespace DesignArch
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<User, UserOrderDto>().ReverseMap();
+            CreateMap<Order, OrderDto>().ReverseMap();
+            CreateMap<Product, ProductDto>().ReverseMap();
+            CreateMap<Product, CreateProductDto>().ReverseMap();
+        }
+    }
+}
